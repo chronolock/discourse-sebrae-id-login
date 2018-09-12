f0a2d0bf7279350cf7be05bfda67fb45d65e3ba8
Vitor <vitor@comunidade.sebraedev.com.br>
2018-09-12 18:55:32 -0300

    Troca de biblioteca para realizar autenticação

--- plugin.rb
+++ plugin.rb
@@ -1,57 +1,166 @@
-# name: sebrae-login
-# about: Plugin responsavel por realizar a autenticação via SebraeID
-# version: 0.0.1
-# authors: Vitor
+# name: discourse-oauth2-basic
+# about: Generic OAuth2 Plugin
+# version: 0.3
+# authors: Robin Ward
+# url: https://github.com/discourse/discourse-oauth2-basic
 
-require 'auth/oauth2_authenticator'
-require 'omniauth-oauth2'
+require_dependency 'auth/oauth2_authenticator.rb'
 
-class HummingbirdAuthenticator < ::Auth::OAuth2Authenticator
+enabled_site_setting :oauth2_enabled
 
-  CCLIENT_ID = '167'
-  CLIENT_SECRET = 'ZCjUMfi1-evtZolCuR9z'
-
-  def register_middleware(omniauth)
-    omniauth.provider :hummingbird, CLIENT_ID, CLIENT_SECRET
-  end
-end
-
-class OmniAuth::Strategies::Hummingbird < OmniAuth::Strategies::OAuth2
-  # Give your strategy a name.
-  option :name, "SebraeID"
-
-  # This is where you pass the options you would pass when
-  # initializing your consumer from the OAuth gem.
-  option :client_options, site: 'https://homolog.sebraeid.sebrae.com.br'
-
-  # These are called after authentication has succeeded. If
-  # possible, you should try to set the UID without making
-  # additional calls (if the user id is returned with the token
-  # or as a URI parameter). This may not be possible with all
-  # providers.
-  uid { raw_info['id'].to_s }
-
+class ::OmniAuth::Strategies::Oauth2Basic < ::OmniAuth::Strategies::OAuth2
+  option :name, "oauth2_basic"
   info do
     {
-      :name => raw_info['FirstName'],
-      :email => raw_info['Email']
+      id: access_token['id']
     }
   end
 
-  extra do
-    {
-      'raw_info' => raw_info
-    }
-  end
-
-  def raw_info
-    @raw_info ||= access_token.get('/OAuth_API/api/Me').parsed
+  def callback_url
+    Discourse.base_url_no_prefix + script_name + callback_path
   end
 end
 
-auth_provider :title => 'Sign in with SebraeID account',
-    :message => 'Log in using your SebraeID account. (Make sure your popup blocker is disabled.)',
-    :frame_width => 920,
-    :frame_height => 800,
-    :authenticator => HummingbirdAuthenticator.new('hummingbird', trusted: true,
-      auto_create_account: true)
+class OAuth2BasicAuthenticator < ::Auth::OAuth2Authenticator
+  def register_middleware(omniauth)
+    omniauth.provider :oauth2_basic,
+                      name: 'oauth2_basic',
+                      setup: lambda { |env|
+                        opts = env['omniauth.strategy'].options
+                        opts[:client_id] = SiteSetting.oauth2_client_id
+                        opts[:client_secret] = SiteSetting.oauth2_client_secret
+                        opts[:provider_ignores_state] = false
+                        opts[:client_options] = {
+                          authorize_url: SiteSetting.oauth2_authorize_url,
+                          token_url: SiteSetting.oauth2_token_url,
+                          token_method: SiteSetting.oauth2_token_url_method.downcase.to_sym
+                        }
+                        opts[:authorize_options] = SiteSetting.oauth2_authorize_options.split("|").map(&:to_sym)
+
+                        if SiteSetting.oauth2_send_auth_header?
+                          opts[:token_params] = { headers: { 'Authorization' => basic_auth_header } }
+                        end
+                        unless SiteSetting.oauth2_scope.blank?
+                          opts[:scope] = SiteSetting.oauth2_scope
+                        end
+                      }
+  end
+
+  def basic_auth_header
+    "Basic " + Base64.strict_encode64("#{SiteSetting.oauth2_client_id}:#{SiteSetting.oauth2_client_secret}")
+  end
+
+  def walk_path(fragment, segments)
+    first_seg = segments[0]
+    return if first_seg.blank? || fragment.blank?
+    return nil unless fragment.is_a?(Hash) || fragment.is_a?(Array)
+    if fragment.is_a?(Hash)
+      deref = fragment[first_seg] || fragment[first_seg.to_sym]
+    else
+      deref = fragment[0] # Take just the first array for now, maybe later we can teach it to walk the array if we need to
+    end
+
+    return (deref.blank? || segments.size == 1) ? deref : walk_path(deref, segments[1..-1])
+  end
+
+  def json_walk(result, user_json, prop)
+    path = SiteSetting.send("oauth2_json_#{prop}_path")
+    if path.present?
+      segments = path.split('.')
+      val = walk_path(user_json, segments)
+      result[prop] = val if val.present?
+    end
+  end
+
+  def log(info)
+    Rails.logger.warn("OAuth2 Debugging: #{info}") if SiteSetting.oauth2_debug_auth
+  end
+
+  def fetch_user_details(token, id)
+    user_json_url = SiteSetting.oauth2_user_json_url.sub(':token', token.to_s).sub(':id', id.to_s)
+    user_json_method = SiteSetting.oauth2_user_json_url_method
+
+    log("user_json_url: #{user_json_method} #{user_json_url}")
+
+    bearer_token = "Bearer #{token}"
+    user_json_response =
+      if user_json_method.downcase.to_sym == :post
+        Net::HTTP
+          .post_form(URI(user_json_url), { 'Authorization' => bearer_token })
+          .body
+      else
+        open(user_json_url, 'Authorization' => bearer_token).read
+      end
+
+    user_json = JSON.parse(user_json_response)
+
+    log("user_json: #{user_json}")
+
+    result = {}
+    if user_json.present?
+      json_walk(result, user_json, :user_id)
+      json_walk(result, user_json, :username)
+      json_walk(result, user_json, :name)
+      json_walk(result, user_json, :email)
+      json_walk(result, user_json, :avatar)
+    end
+
+    result
+  end
+
+  def after_authenticate(auth)
+    log("after_authenticate response: \n\ncreds: #{auth['credentials'].to_hash}\ninfo: #{auth['info'].to_hash}\nextra: #{auth['extra'].to_hash}")
+
+    result = Auth::Result.new
+    token = auth['credentials']['token']
+    user_details = fetch_user_details(token, auth['info'][:id])
+
+    result.name = user_details[:name]
+    result.username = user_details[:username]
+    result.email = user_details[:email]
+    result.email_valid = result.email.present? && SiteSetting.oauth2_email_verified?
+    avatar_url = user_details[:avatar]
+
+    current_info = ::PluginStore.get("oauth2_basic", "oauth2_basic_user_#{user_details[:user_id]}")
+    if current_info
+      result.user = User.where(id: current_info[:user_id]).first
+    elsif SiteSetting.oauth2_email_verified?
+      result.user = User.find_by_email(result.email)
+      if result.user && user_details[:user_id]
+        ::PluginStore.set("oauth2_basic", "oauth2_basic_user_#{user_details[:user_id]}", user_id: result.user.id)
+      end
+    end
+
+    Jobs.enqueue(:download_avatar_from_url,
+      url: avatar_url,
+      user_id: result.user.id,
+      override_gravatar: SiteSetting.sso_overrides_avatar
+    ) if result.user && avatar_url.present?
+
+    result.extra_data = { oauth2_basic_user_id: user_details[:user_id] }
+    result
+  end
+
+  def after_create_account(user, auth)
+    ::PluginStore.set("oauth2_basic", "oauth2_basic_user_#{auth[:extra_data][:oauth2_basic_user_id]}", user_id: user.id)
+  end
+
+  def enabled?
+    SiteSetting.oauth2_enabled
+  end
+end
+
+auth_provider title_setting: "oauth2_button_title",
+              enabled_setting: "oauth2_enabled",
+              authenticator: OAuth2BasicAuthenticator.new('oauth2_basic'),
+              message: "OAuth2",
+              full_screen_login_setting: "oauth2_full_screen_login"
+
+
+register_css <<CSS
+
+  button.btn-social.oauth2_basic {
+    background-color: #6d6d6d;
+  }
+
+CSS
---
+++ spec/plugin_spec.rb
@@ -0,0 +1,112 @@
+require 'rails_helper'
+require 'json'
+
+# This is ugly... but it works!
+# Need to load plugin.rb to avoid:
+#
+# NameError:
+#   uninitialized constant OAuth2BasicAuthenticator
+#
+# And need to mock various methods to avoid:
+#
+# NoMethodError:
+#   undefined method `enabled_site_setting' for main:Object
+#
+# etc.
+
+def enabled_site_setting(arg)
+end
+
+def auth_provider(arg)
+end
+
+def register_css(arg)
+end
+
+require_relative '../plugin.rb'
+
+describe OAuth2BasicAuthenticator do
+  context 'after_authenticate' do
+    let(:user) { Fabricate(:user) }
+    let(:authenticator) { OAuth2BasicAuthenticator.new('oauth2_basic') }
+
+    let(:auth) do
+      { 'credentials' => { 'token': 'token' },
+        'info' => { id: 'id' },
+        'extra' => {} }
+    end
+
+    before(:each) do
+      SiteSetting.oauth2_email_verified = true
+    end
+
+    it 'finds user by email' do
+      authenticator.expects(:fetch_user_details).returns(email: user.email)
+      result = authenticator.after_authenticate(auth)
+      expect(result.user).to eq(user)
+    end
+
+    context 'avatar downloading' do
+      before { SiteSetting.queue_jobs = true }
+
+      let(:job_klass) { Jobs::DownloadAvatarFromUrl }
+
+      before do
+        png = Base64.decode64("R0lGODlhAQABALMAAAAAAIAAAACAAICAAAAAgIAAgACAgMDAwICAgP8AAAD/AP//AAAA//8A/wD//wBiZCH5BAEAAA8ALAAAAAABAAEAAAQC8EUAOw==")
+        stub_request(:get, "http://avatar.example.com/avatar.png").to_return(body: png, headers: { "Content-Type" => "image/png" })
+      end
+
+      it 'enqueues a download_avatar_from_url job' do
+        authenticator.expects(:fetch_user_details).returns(
+          email: user.email,
+          avatar: 'http://avatar.example.com/avatar.png'
+        )
+        expect {
+          authenticator.after_authenticate(auth)
+        }.to change { job_klass.jobs.count }.by(1)
+
+        job_args = job_klass.jobs.last['args'].first
+
+        expect(job_args['url']).to eq("http://avatar.example.com/avatar.png")
+        expect(job_args['user_id']).to eq(user.id)
+        expect(job_args['override_gravatar']).to eq(false)
+      end
+    end
+  end
+
+  it 'can walk json' do
+    authenticator = OAuth2BasicAuthenticator.new('oauth2_basic')
+    json_string = '{"user":{"id":1234,"email":{"address":"test@example.com"}}}'
+    SiteSetting.oauth2_json_email_path = 'user.email.address'
+    result = authenticator.json_walk({}, JSON.parse(json_string), :email)
+
+    expect(result).to eq "test@example.com"
+  end
+
+  it 'can walk json that contains an array' do
+    authenticator = OAuth2BasicAuthenticator.new('oauth2_basic')
+    json_string = '{"email":"test@example.com","identities":[{"user_id":"123456789","provider":"auth0","isSocial":false}]}'
+    SiteSetting.oauth2_json_user_id_path = 'identities.[].user_id'
+    result = authenticator.json_walk({}, JSON.parse(json_string), :user_id)
+
+    expect(result).to eq "123456789"
+  end
+
+  it 'can walk json and handle an empty array' do
+    authenticator = OAuth2BasicAuthenticator.new('oauth2_basic')
+    json_string = '{"email":"test@example.com","identities":[]}'
+    SiteSetting.oauth2_json_user_id_path = 'identities.[].user_id'
+    result = authenticator.json_walk({}, JSON.parse(json_string), :user_id)
+
+    expect(result).to eq nil
+  end
+
+  it 'can walk json and download avatar' do
+    authenticator = OAuth2BasicAuthenticator.new('oauth2_basic')
+    json_string = '{"user":{"avatar":"http://example.com/1.png"}}'
+    SiteSetting.oauth2_json_avatar_path = 'user.avatar'
+    result = authenticator.json_walk({}, JSON.parse(json_string), :avatar)
+
+    expect(result).to eq 'http://example.com/1.png'
+  end
+end
