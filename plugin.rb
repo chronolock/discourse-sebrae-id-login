3ae2c32b1c2adaa560b3bad206a93be305632428
Vitor <vitor@comunidade.sebraedev.com.br>
2018-09-12 18:51:13 -0300

    Novo teste utilizando Oauth2

--- plugin.rb
+++ plugin.rb
@@ -3,72 +3,55 @@
 # version: 0.0.1
 # authors: Vitor
 
-gem 'omniauth-vkontakte', '1.3.3'
+require 'auth/oauth2_authenticator'
+require 'omniauth-oauth2'
 
+class HummingbirdAuthenticator < ::Auth::OAuth2Authenticator
 
-class SebraeIDAuthenticator < ::Auth::Authenticator
-
-  CLIENT_ID = '167'
+  CCLIENT_ID = '167'
   CLIENT_SECRET = 'ZCjUMfi1-evtZolCuR9z'
 
-  def name
-    'SebraeID'
-  end
-
-  def after_authenticate(auth_token)
-    result = Auth::Result.new
-
-    # grap the info we need from omni auth
-    data = auth_token[:info]
-    name = data["FirstName"]
-    mv_uid = auth_token["ID"]
-    email = data['Email']
-
-    # plugin specific data storage
-    current_info = ::PluginStore.get("sID", "mv_uid_#{mv_uid}")
-
-    result.user =
-      if current_info
-        User.where(id: current_info[:user_id]).first
-      end
-
-    result.name = name
-    result.extra_data = { mv_uid: mv_uid }
-    result.email = email
-
-    result
-  end
-
-  def after_create_account(user, auth)
-    data = auth[:extra_data]
-    ::PluginStore.set("sID", "mv_uid_#{data[:mv_uid]}", {user_id: user.id })
-  end
-
   def register_middleware(omniauth)
-    omniauth.provider :SebraeID,
-     CLIENT_ID,
-     CLIENT_SECRET
+    omniauth.provider :hummingbird, CLIENT_ID, CLIENT_SECRET
   end
 end
 
+class OmniAuth::Strategies::Hummingbird < OmniAuth::Strategies::OAuth2
+  # Give your strategy a name.
+  option :name, "SebraeID"
 
-auth_provider :title => 'with SebraeID Accounts',
-    :message => 'Login via SebraeID',
+  # This is where you pass the options you would pass when
+  # initializing your consumer from the OAuth gem.
+  option :client_options, site: 'https://homolog.sebraeid.sebrae.com.br'
+
+  # These are called after authentication has succeeded. If
+  # possible, you should try to set the UID without making
+  # additional calls (if the user id is returned with the token
+  # or as a URI parameter). This may not be possible with all
+  # providers.
+  uid { raw_info['id'].to_s }
+
+  info do
+    {
+      :name => raw_info['FirstName'],
+      :email => raw_info['Email']
+    }
+  end
+
+  extra do
+    {
+      'raw_info' => raw_info
+    }
+  end
+
+  def raw_info
+    @raw_info ||= access_token.get('/OAuth_API/api/Me').parsed
+  end
+end
+
+auth_provider :title => 'Sign in with SebraeID account',
+    :message => 'Log in using your SebraeID account. (Make sure your popup blocker is disabled.)',
     :frame_width => 920,
     :frame_height => 800,
-    :authenticator => SebraeIDAuthenticator.new
-
-
-# We ship with zocial, it may have an icon you like http://zocial.smcllns.com/sample.html
-#  in our current case we have an icon for vk
-register_css <<CSS
-
-.btn-social.vkontakte {
-  background: #46698f;
-}
-
-.btn-social.vkontakte:before {
-  content: "N";
-}
-
-CSS
+    :authenticator => HummingbirdAuthenticator.new('hummingbird', trusted: true,
+      auto_create_account: true)
